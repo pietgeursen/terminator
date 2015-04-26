d2e2adcb3a48160040d4c6186dbcdc335a62ce97
Piet Geursen <pietgeursen@gmail.com>
2015-04-26 15:34:25 +1200

    Getting json colours of random sunset image!

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,21 +16,18 @@ end
 
 get '/colours' do
 
-	#get instagram links by tag.
 	client = Instagram.client(:access_token => session[:access_token])
-	tags = client.tag_search('cat')
-	puts tags
 
+	sunset_url = client.tag_recent_media('sunset')[0].images.thumbnail.url
+	puts sunset_url
 
+	response = Unirest.get "https://apicloud-colortag.p.mashape.com/tag-url.json",
+	    headers:{
+	      "X-Mashape-Key" => ENV['COLORTAG_MASHAPE_KEY'],
+	      "Accept" => "application/json"
+	    },
+	    parameters:{:palette =>"precise", :sort=>"weight", :url=>sunset_url}
 
-	  # These code snippets use an open-source library. http://unirest.io/ruby
-	#   response = Unirest.get "https://apicloud-colortag.p.mashape.com/tag-url.json",
-	#     headers:{
-	#       "X-Mashape-Key" => ENV['COLORTAG_MASHAPE_KEY'],
-	#       "Accept" => "application/json"
-	#     },
-	#     parameters:{:palette =>"precise", :sort=>"weight", :url=>"https://awesomewallpapers.files.wordpress.com/2014/01/triforce.jpg"}
-
-	# response.body.to_json
+	response.body.to_json
 
 end
