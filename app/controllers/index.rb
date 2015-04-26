c1fe3aee8f8ddc9623a00451ba0d1bc067e3b0fa
Piet Geursen <pietgeursen@gmail.com>
2015-04-26 13:20:07 +1200

    Can get colour schemes from colourtag api.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,4 +8,15 @@ get '/colours' do
 
 	#get instagram links by tag.
 
+
+	  # These code snippets use an open-source library. http://unirest.io/ruby
+	  response = Unirest.get "https://apicloud-colortag.p.mashape.com/tag-url.json",
+	    headers:{
+	      "X-Mashape-Key" => ENV['COLORTAG_MASHAPE_KEY'],
+	      "Accept" => "application/json"
+	    },
+	    parameters:{:palette =>"precise", :sort=>"weight", :url=>"https://awesomewallpapers.files.wordpress.com/2014/01/triforce.jpg"}
+
+	response.body.to_json
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ require 'erb'
 require 'instagram'
 require 'unirest'
 require 'dotenv'
+Dotenv.load
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
