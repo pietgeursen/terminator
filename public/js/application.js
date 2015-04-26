34e7c7282a6de9f3996b961d956adbc05c4d0150
Piet Geursen <pietgeursen@gmail.com>
2015-04-26 17:25:13 +1200

    Basic idea works!

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	var coloursArray = [];
+
+	$.getJSON("/colours", function(data){
+		coloursArray = data.tags;
+		
+		appendColouredDivs(coloursArray);
+	})
+
+	function appendColouredDivs(colours){
+		for(var i = 0; i < colours.length; i++){
+			colour = colours[i].color;
+			console.log(colour);
+			var $newDiv = $("<div class='colour_box'></div>")
+			$newDiv.css('background-color', colour);
+			$newDiv.css('height', 100/colours.length +"%")
+			$('#terminator').append($newDiv);
+		}
+	}
+
+	
+	
+
 });
